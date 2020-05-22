965a15f83a82d42b83fae61f845741ab1e403d2f
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-05-22 22:49:09 +0900

    エネミースポナーの下地を作成

---
+++ Assets/ggd/Scripts/Spawner/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Spawner
+{
+    /// <summary>
+    /// エネミーのスポナー.
+    /// </summary>
+    public class EnemySpawner : MonoBehaviour
+    {
+
+
+        private IEnumerator Spawn()
+        {
+            yield return null;
+        }
+
+
+
+        public void StartSpawnScedule()
+        {
+        }
+
+
+        
+
+
+    }
+}
