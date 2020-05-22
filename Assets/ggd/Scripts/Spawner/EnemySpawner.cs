a365d5688f4beda76dac492088801d7b05029cdb
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-05-22 23:27:22 +0900

    エネミーのスポーンするサンプルを作成

--- Assets/ggd/Scripts/Spawner/EnemySpawner.cs
+++ Assets/ggd/Scripts/Spawner/EnemySpawner.cs
@@ -9,16 +9,41 @@ namespace Spawner
     public class EnemySpawner : MonoBehaviour
     {
 
+        // TODO : とりあえずこの実装、最終的にはScriptableObjectなりで実装する.
+        [SerializeField] GameObject enemyPrefab;
 
-        private IEnumerator Spawn()
+
+        void Start()
         {
-            yield return null;
+            // TODO : 最終的には外部からの軌道にする.
+            StartSpawnScedule();
         }
 
+        
+        private IEnumerator Spawn()
+        {
+            bool isComplete = false;
+            float timeCount = 0f;
+            int spawnCount = 0;
+            while (!isComplete)
+            {
+                yield return new WaitUntil(
+                    () =>
+                    {
+                        timeCount += Time.deltaTime;
+                        return 1f <= timeCount;
+                    });
+                timeCount = 0f;
+                Instantiate(enemyPrefab);
+                ++spawnCount;
+                isComplete = 10 <= spawnCount;
+            }
+        }
 
-
+        
         public void StartSpawnScedule()
         {
+            StartCoroutine(Spawn());
         }
 
 
